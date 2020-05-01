4b84c5c5d8a923e1958535cdb58d97676932ca62
dsafawef <dsafawef@adkfne.com>
2020-05-01 12:37:02 +0900

    2020/05/01 Ch02 등록

---
+++ Ch02/src/sub2/DataTypeTest.java
@@ -0,0 +1,16 @@
+package sub2;
+
+/*
+ * 날짜 : 2020/04/29
+ * 이름 : 신동호
+ * 내용 : 변수의 자료형(데이터 타입) 실습하기
+ */
+public class DataTypeTest {
+	
+	public static void main(String[] args) {
+		
+		// 정수형
+		byte var1 =  
+	}
+
+}
