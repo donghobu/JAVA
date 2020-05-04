5ced17f9a2348f962ba1330f04e4e99fc1b41c3e
dsafawef <dsafawef@adkfne.com>
2020-05-04 12:25:48 +0900

    2020/05/04 MultiArrayTest 추가

--- Ch05/src/sub1/MultiArrayTest.java
+++ Ch05/src/sub1/MultiArrayTest.java
@@ -27,7 +27,7 @@ public class MultiArrayTest {
 		
 		for(int a=0 ; a<arr2d.length ; a++ )  { // 행
 			
-			for(int b=0 ; b<arr2d[a].length ; b++ ) {
+			for(int b=0 ; b<arr2d[a].length ; b++ ) { // 열
 				System.out.println("배열 arr2d의 "+(a+1)+"행 "+(b+1)+"열 원소: "+arr2d[a][b]);
 			}
 		}
@@ -45,6 +45,26 @@ public class MultiArrayTest {
 		System.out.println("배열 arr2d의 3행 2열 원소 : "+arr2d[2][1]);
 		System.out.println("배열 arr2d의 3행 3열 원소 : "+arr2d[2][2]);
 		System.out.println("배열 arr2d의 3행 4열 원소 : "+arr2d[2][3]);
+		
 		// 3차원 배열
+		int arr3d[][][] = {{{1, 2, 3},
+			                {4, 5, 6}, 
+			                {7, 8, 9}, 
+			                {10, 11, 12}},
+				           {{13, 14, 15}, 
+			                {16, 17, 18}, 
+			                {19, 20, 21}, 
+			                {22, 23, 24}},
+				           {{25, 26, 27}, 
+			                {28, 29, 30}, 
+			                {31, 32, 33}, 
+			                {34, 35, 36}}}; // 행, 열, 면
+		
+		System.out.println("배열 arr3d의 1면1행1열 : "+arr3d[0][0][0]);
+		System.out.println("배열 arr3d의 1면3행3열 : "+arr3d[0][2][2]);
+		System.out.println("배열 arr3d의 2면1행2열 : "+arr3d[1][0][1]);
+		System.out.println("배열 arr3d의 2면3행3열 : "+arr3d[1][2][2]);
+		System.out.println("배열 arr3d의 3면1행3열 : "+arr3d[2][0][2]);
+		System.out.println("배열 arr3d의 3면4행3열 : "+arr3d[2][3][2]);
 	}
 }
