c6c74aa4d408ac374dd076b2f94ae717ab3edafe
dsafawef <dsafawef@adkfne.com>
2020-05-07 17:15:24 +0900

    2020/05/07 내용 추가

---
+++ JavaTest2/src/test01/JavaTest1.java
@@ -0,0 +1,24 @@
+package test01;
+/*
+ * 날짜 : 2020/05/07
+ * 이름 : 신동호
+ * 내용 : 배열 문자 출력하기
+ */
+public class JavaTest1 {
+	public static void main(String[] args) {
+		
+		char str[] = {'I',' ', 'L','O','V','E', ' ', 'Y','O','U'};
+		int row, col;
+		
+		for (row = 1; row < 10; row++) {
+			
+			for (col = 0; col <= row; col++) {
+				
+				System.out.print(" ");
+			}
+			
+			System.out.print("\n");
+		}
+	}
+
+}
