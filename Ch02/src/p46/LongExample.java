a58cbbb9c25587bdbc5c33d13de5972c44bca2c0
dsafawef <dsafawef@adkfne.com>
2020-05-08 11:11:27 +0900

    2020/05/07 교재 내용 실습 추가

---
+++ Ch02/src/p46/LongExample.java
@@ -0,0 +1,18 @@
+package p46;
+/*
+ * 작성자 : 신동호
+ * 작성일 : 2020.05.07
+ */
+public class LongExample {
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		//long var3 = 1000000000000;     // 컴파일 에러
+		long var4 = 1000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	}
+
+}
