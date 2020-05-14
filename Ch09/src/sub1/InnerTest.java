1c4c92efde964a3668f641ff7b82152767f1cdb3
dsafawef <dsafawef@adkfne.com>
2020-05-14 11:39:44 +0900

    2020/05/14 예외 처리 내용 추가

--- Ch09/src/sub1/InnerTest.java
+++ Ch09/src/sub1/InnerTest.java
@@ -1,5 +1,20 @@
 package sub1;
 
+import sub1.Outer.Inner;
+
+/*
+ * 날짜 : 2020/05/14
+ * 이름 : 신동호
+ * 내용 : 중첩클래스 실습하기 교재 p390
+ */
 public class InnerTest {
+	public static void main(String[] args) {
+		
+		Outer out = new Outer(10);
+		out.info();
+		
+		Inner inner = out.new Inner(20);
+		inner.info();
+	}
 
 }
--- Ch09/src/sub2/Person.java
+++ Ch09/src/sub2/Person.java
@@ -1,5 +1,10 @@
 package sub2;
-
+/**
+ * 날짜 : 2020/05/14
+ * 이름 : 신동호
+ * 내용 : 익명객체 실습하기 교재 p404
+ */
 public interface Person {
-
+	public abstract void hello();
+	public void info();
 }
