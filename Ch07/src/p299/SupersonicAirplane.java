b2e190e0587e46fdd97123b3b3ba2b606e9b9604
dsafawef <dsafawef@adkfne.com>
2020-05-19 17:34:35 +0900

    2020/05/19 Ch07 교재 예제 실습 추가

--- Ch07/src/p299/SupersonicAirplane.java
+++ Ch07/src/p299/SupersonicAirplane.java
@@ -1,6 +1,6 @@
 package p299;
 
-public class SupersonicAirplane {
+public class SupersonicAirplane extends Airplane {
 	public static final int NORMAL = 1;
 	public static final int SUPERSONIC = 2;
 	
